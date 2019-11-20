e613662c79dd425cd16197cadcbe58e4c412d920
joaohenriquef <joaohenriquef@poli.ufrj.br>
2019-11-20 15:58:51 -0300

    Working tsv parsing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,8 @@ async function handleLoad() {
   const data = await d3.tsv("../../assets/data/data.tsv");
   const item = getRandomItem(data);
 
-  d3.select(".tip")
-
-  const tipDiv = document.getElementById("tip")
-  tipDiv.innerText = item.tip;
+  d3.select(".tip-text").html(item.tip);
+  d3.select(".sub-topic").text(` / ${item.subject}`)
 }
 
 window.onload = handleLoad;
